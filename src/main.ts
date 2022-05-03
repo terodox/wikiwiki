47e12d59ee4c5f00b37a0a897f668e3af8acb6fd
Andrew Desmarais <andy.desmarais@meltwater.com>
2022-05-03 16:18:26 -0400

    Game is now functional

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import './components/letter-display';
-import './components/sentence-display';
+import { attributes, SentenceDisplay } from './components/sentence-display';
+
+const sentence = "Mary had a little lamb who's fleece was white as snow";
+let currentIndex = 0;
+const sentenceTag = document.querySelector(SentenceDisplay.tag)!;
+sentenceTag.setAttribute(attributes.sentence, sentence);
 
 document.addEventListener('keypress', (event) => {
   console.log(event.key);
+  sentenceTag.removeAttribute(attributes.isFailing);
+  if (event.key === sentence[currentIndex]) {
+    currentIndex++;
+    sentenceTag.setAttribute(attributes.currentIndex, currentIndex.toString());
+  } else {
+    sentenceTag.setAttribute(attributes.isFailing, '');
+  }
 });
 
 export {};
